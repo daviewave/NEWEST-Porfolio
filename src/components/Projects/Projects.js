03043774458b04326d466648356613e3cbbb2fdf
David Silveira <daviewave@gmail.com>
2022-01-06 01:36:54 -0500

    tested how the projects were listed, then moved array to another folder called constraints, that we just import instead, looks a lot cleaner

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import { currentProjects } from "../../constants/constants";
 
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
     <GridContainer>
-      {["Project 1", "Project 2", "Project 3"].map((project) => (
-        <div>{project}</div>
+      {currentProjects.map((project) => (
+        <div>{project.title}</div>
       ))}
     </GridContainer>
   </Section>
 );
+
+
 export default Projects;
