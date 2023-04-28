b4ffcf8ae62a015cff459e38c40b03fc112eb6c2
David Silveira <davidsilveira@Davids-MacBook-Pro.local>
2023-04-28 18:04:17 -0400

    updated technologies section to better reflect my current tech stack

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,62 +1,107 @@
 import React from 'react';
-import { DiCloud9, DiDatabase, DiReact } from "react-icons/di";
+import { DiCloud9, DiDatabase, DiReact } from 'react-icons/di';
 import {
-  Section,
+  TechSection,
   SectionDivider,
   SectionText,
   SectionTitle,
-} from "../../styles/GlobalComponents";
+} from '../../styles/GlobalComponents';
 import {
   List,
   ListContainer,
   ListItem,
   ListParagraph,
   ListTitle,
-} from "./TechnologiesStyles";
+} from './TechnologiesStyles';
 
 const Technologies = () => (
-  <Section id="tech">
+  <TechSection id="tech">
     <SectionDivider />
     <br />
     <SectionTitle>Technologies</SectionTitle>
     <SectionText>
-      I have worked with a variety of technologies in both the front end & UX/UI
-      development, and also through the backend server development.
+      I have worked with a variety of technologies in the software engineering
+      field, focusing on web development, data analysis, and machine learning.
     </SectionText>
     <List>
       <ListItem>
-        <DiReact size="3rem" />
         <ListContainer>
-          <ListTitle>Front-End</ListTitle>
+          <div style={{ display: 'flex' }}>
+            <ListTitle>Front-End</ListTitle> <br />
+            <div style={{ marginLeft: '3%' }}>
+              <DiReact size="3rem" />
+            </div>
+          </div>
+
           <ListParagraph>
-            Experience with: <br />- React.js
-            <br /> - NextJS <br /> - Handlebars.js <br /> - Bootstrap <br /> -
-            CSS
+            - React.js
+            <br />- JavaScript
+            <br />- CSS
+            <br />- HTML
+            <br />- Material-UI
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <DiDatabase size="3rem" />
         <ListContainer>
-          <ListTitle>Back-End</ListTitle>
+          <div style={{ display: 'flex' }}>
+            <ListTitle>Back-End</ListTitle> <br />
+            <div style={{ marginLeft: '3%' }}>
+              <DiDatabase size="3rem" />
+            </div>
+          </div>
+
           <ListParagraph>
-            Experience with: <br />- Node.js <br />- Java <br />- C
-            <br /> - MySql <br /> - NoSql <br /> - Mongo
-            <br /> - Express.js <br /> - Apollo <br /> - graphql
+            - Python
+            <br />- Django
+            <br />- Django Rest Framework
+            <br />- SQL
+            <br />- Postgres
+            <br />- Java
+            <br />- C
+            <br />- Data Structures
+            <br />- Shell Scripting
+            <br />- R
           </ListParagraph>
         </ListContainer>
       </ListItem>
       <ListItem>
-        <DiCloud9 size="3rem" />
         <ListContainer>
-          <ListTitle>UX/UI</ListTitle>
+          <div style={{ display: 'flex' }}>
+            <ListTitle>DevOps</ListTitle>
+            <div style={{ marginLeft: '3%' }}>
+              <DiCloud9 size="3rem" />
+            </div>
+          </div>
+
           <ListParagraph>
-            Experience with: <br />- Figma
+            - Linux
+            <br />- Docker
+            <br />- Node.js
+            <br />- Git
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <ListContainer>
+          <div style={{ display: 'flex' }}>
+            <ListTitle>ML & Others</ListTitle>
+            <div style={{ marginLeft: '3%' }}>
+              <DiReact size="3rem" />
+            </div>
+          </div>
+
+          <ListParagraph>
+            - GPT Models
+            <br />- Transformer Models
+            <br />- Word2Vec
+            <br />- Embeddings
+            <br />- Reinforcement Learning
           </ListParagraph>
         </ListContainer>
       </ListItem>
     </List>
-  </Section>
+  </TechSection>
 );
 
 export default Technologies;
