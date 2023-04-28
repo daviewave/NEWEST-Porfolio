24c44654c6bd4280ea1c4d339d712f0dce4c5883
David Silveira <davidsilveira@Davids-MacBook-Pro.local>
2023-04-28 18:00:33 -0400

    updated Timeline constant

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,66 +1,80 @@
 export const currentProjects = [
   {
-    title: "E-commerce Sample Site",
+    title: 'E-commerce Sample Site',
     description:
-      "A sample e-commerce website deployed on aws elastic beanstalk",
-    image: "/images/Ecommerce.png",
-    tags: ["Django", "Python", "AWS"],
-    source: "http://ecommerce-django-dev.us-east-1.elasticbeanstalk.com/",
-    visit: "https://github.com/daviewave/ecommerce-course",
+      'A sample e-commerce website deployed on aws elastic beanstalk',
+    image: '/images/Ecommerce.png',
+    tags: ['Django', 'Python', 'AWS'],
+    source: 'http://ecommerce-django-dev.us-east-1.elasticbeanstalk.com/',
+    visit: 'https://github.com/daviewave/ecommerce-course',
     id: 0,
   },
   {
-    title: "Foode",
+    title: 'Foode',
     description:
-      "Enter a the ingredients you currently have, and instantly get recipes for meals with the ingredients you have",
-    image: "/images/recipes-project-photo.jpg",
-    tags: ["HTML", "CSS", "JavaScript"],
-    source: "https://mwathomas.github.io/project-1/",
-    visit: "https://github.com/mwathomas/project-1",
+      'Enter a the ingredients you currently have, and instantly get recipes for meals with the ingredients you have',
+    image: '/images/recipes-project-photo.jpg',
+    tags: ['HTML', 'CSS', 'JavaScript'],
+    source: 'https://mwathomas.github.io/project-1/',
+    visit: 'https://github.com/mwathomas/project-1',
     id: 0,
   },
   {
-    title: "My Interactions",
+    title: 'My Interactions',
     description:
-      "If your someone who likes to take a lot of vitamins, supplements, or any drugs in general this application will be your friend. Get interactions for any drug",
-    image: "/images/Drug-Interactions-photo.jpg",
-    tags: ["MySql", "Express", "Node.js"],
-    source: "https://my-interactions.herokuapp.com/",
-    visit: "https://github.com/aellis07/my-interactions",
+      'If your someone who likes to take a lot of vitamins, supplements, or any drugs in general this application will be your friend. Get interactions for any drug',
+    image: '/images/Drug-Interactions-photo.jpg',
+    tags: ['MySql', 'Express', 'Node.js'],
+    source: 'https://my-interactions.herokuapp.com/',
+    visit: 'https://github.com/aellis07/my-interactions',
     id: 1,
   },
   {
-    title: "Prop-Swap",
+    title: 'Prop-Swap',
     description:
-      "Love to travel, hate the price tag? You could potentially split your travelling cost in half by using prop-swap to swap your own property with someone travelling to your area!",
-    image: "/images/Prop-swap-photo.jpg",
-    tags: ["Mongo", "Express.js", "React.js", "Node.js"],
-    source: "https://prop-swapper.herokuapp.com/",
-    visit: "https://github.com/daviewave/Prop-Swapper",
+      'Love to travel, hate the price tag? You could potentially split your travelling cost in half by using prop-swap to swap your own property with someone travelling to your area!',
+    image: '/images/Prop-swap-photo.jpg',
+    tags: ['Mongo', 'Express.js', 'React.js', 'Node.js'],
+    source: 'https://prop-swapper.herokuapp.com/',
+    visit: 'https://github.com/daviewave/Prop-Swapper',
     id: 2,
   },
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: "Graduated High School, with honors." },
-  { year: 2018, text: "Entered UMass Amherst to begin studying Psychology." },
+  {
+    year: 2017,
+    text: 'Graduated High School, with honors. During this time, I developed an interest in Neuroscience and looked forward to exploring the field in college.',
+  },
+  {
+    year: 2018,
+    text: 'Entered UMass Amherst to begin studying Psychology. As part of my Neuroscience major requirements, I had to take a computer science class. I chose Intro to Programming, where I initially struggled but persevered with the help of my software developer dad, who taught me the basics and ignited my passion for the field.',
+  },
   {
     year: 2019,
-    text: "Began a concentration in Neuroscience within my Psychology major. A requirement to fufill the Neuroscience specialty could be fufilled by either taking Intro to calculus or Intro to computer science. I chose the latter.",
+    text: 'Began a concentration in Neuroscience within my Psychology major. A requirement to fulfill the Neuroscience specialty could be fulfilled by either taking Intro to Calculus or Intro to Computer Science. I chose the latter.',
   },
   {
     year: 2020,
-    text: "Continued in the Neuroscience field while also taking several challenging computer science courses such as programming with data structures, computer system principles, and intro to Computation theory.",
+    text: 'Continued in the Neuroscience field while also taking several challenging computer science courses such as programming with data structures, computer system principles, and intro to Computation theory.',
   },
   {
     year: 2021,
-    text: "Graduated from UMass with a Bachelors Degree in Neuroscience. In order to further develop and solidify my coding skills, I enrolled and completed a full time coding bootcamp course through Columbia University to become a certified full stack engineer.",
+    text: 'Graduated from UMass with a Bachelors Degree in Neuroscience. In order to further develop and solidify my coding skills, I enrolled and completed a full time coding bootcamp course through Columbia University to become a certified full stack engineer.',
+  },
+  {
+    year: 2022,
+    text: 'Started working as a Software Engineer at Forum Systems, Boston, where I collaborated with top executives to design and execute a cutting-edge machine learning-driven risk analysis feature for PDF contracts.',
+  },
+  {
+    year: 2023,
+    text: 'Continued working at Forum Systems, where I spearheaded a team of developers and played a pivotal role in building a healthcare-specific ML application using Django and Reactjs, as well as onboarding new software engineers.',
   },
 ];
 
 export const accomplishments = [
-  { number: 2, text: "Degrees/Certifcations" },
-  { number: 34, text: "GitHub Repositories" },
-  { number: 5, text: "Coding Languages" },
-  { number: 1, text: "Research Projects" },
+  { number: 2, text: 'Degrees/Certifcations' },
+  { number: 34, text: 'GitHub Repositories' },
+  { number: 5, text: 'Coding Languages' },
+  { number: 1, text: 'Research Projects' },
 ];
