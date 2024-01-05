6c8aa0ab3fe95775b71ea52ebd3d3b029fd5d89a
David Silveira <daviewave@gmail.com>
2024-01-05 16:03:55 -0500

    small fix

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -9,7 +9,7 @@ export const Img = styled.img`
 
 export const GridContainer = styled.section`
 display: grid;
-grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+grid-template-columns: repeat(2, minmax(400px, 1fr));
 padding: 3rem;
 place-items: center;
 column-gap: 2rem;
