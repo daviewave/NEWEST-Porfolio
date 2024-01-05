3b2756010532ce390e1fbee0f3a0e0e6f785fa68
David Silveira <daviewave@gmail.com>
2024-01-05 15:55:05 -0500

    import error

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
+import Projects from '../components/Projects/Projects';
 
 const Home = () => {
   return (
